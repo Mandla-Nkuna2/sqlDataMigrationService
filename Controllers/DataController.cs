20f7e2d947f80ce6cb0e1062414bc0171877bc61
b.sass@innovativethinking.co.za <b.sass@innovativethinking.co.za>
2021-03-25 08:51:15 +0200

    basic functionality

---
+++ Controllers/DataController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dataMigrationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        public string GetAll()
+        {
+            return "Add connection string to body";
+        }
+
+        [HttpGet("{connString}")]
+        public string Get([FromBody] string connString)
+        {
+            return connString;
+        }
+    }
+}
